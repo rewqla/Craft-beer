a57aa48d6e812b9778343a07c0cad68575642370
rewqla <desares526@gmail.com>
2023-09-08 15:40:56 +0300

    Hotfix seeder

--- Craft-beer-backend/Entities/SeederDB.cs
+++ Craft-beer-backend/Entities/SeederDB.cs
@@ -14,79 +14,84 @@ namespace OilShop.Entities
         public static void SeedData(IServiceProvider services,
             IWebHostEnvironment env, IConfiguration config)
         {
-            using (var scope = services.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            try
             {
-                var manager = scope.ServiceProvider.GetRequiredService<UserManager<DbUser>>();
-                var managerRole = scope.ServiceProvider.GetRequiredService<RoleManager<DbRole>>();
-                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-
-                var roleName = "Admin";
-                var result = managerRole.CreateAsync(new DbRole
+                using (var scope = services.GetRequiredService<IServiceScopeFactory>().CreateScope())
                 {
-                    Name = roleName
-                }).Result;
+                    var manager = scope.ServiceProvider.GetRequiredService<UserManager<DbUser>>();
+                    var managerRole = scope.ServiceProvider.GetRequiredService<RoleManager<DbRole>>();
+                    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
-                roleName = "User";
-                result = managerRole.CreateAsync(new DbRole
-                {
-                    Name = roleName
-                }).Result;
+                    var roleName = "Admin";
+                    var result = managerRole.CreateAsync(new DbRole
+                    {
+                        Name = roleName
+                    }).Result;
 
-                string email = "admin@gmail.com";
-                var user = new DbUser
-                {
-                    Email = email,
-                    UserName = email,
-                    FullName = "Stepan New",
-                    PhoneNumber = "+380974355566"
-                };
-                result = manager.CreateAsync(user, "12345678").Result;
-                result = manager.AddToRoleAsync(user, roleName).Result;
+                    roleName = "User";
+                    result = managerRole.CreateAsync(new DbRole
+                    {
+                        Name = roleName
+                    }).Result;
 
-                email = "user@gmail.com";
-                user = new DbUser
-                {
-                    Email = email,
-                    UserName = email,
-                    FullName = "Hidan New",
-                    PhoneNumber = "+380974355116"
-                };
-                result = manager.CreateAsync(user, "12345678").Result;
-                result = manager.AddToRoleAsync(user, roleName).Result;
+                    string email = "admin@gmail.com";
+                    var user = new DbUser
+                    {
+                        Email = email,
+                        UserName = email,
+                        FullName = "Stepan New",
+                        PhoneNumber = "+380974355566"
+                    };
+                    result = manager.CreateAsync(user, "12345678").Result;
+                    result = manager.AddToRoleAsync(user, roleName).Result;
 
-                context.CraftBeers.Add(new CraftBeer
-                {
-                    Name = "Bud",
-                    Price = 40,
-                    Volume = 0.5,
-                    ABV = 5.0,
-                    Description = "Пиво Bud – всесвітньо відомий король серед пива був вперше представлений Адольфусом Бушем ще у 1876 році. Відтоді й досі BUD вариться з дотриманням високих стандартів якості, незмінних з плином часу.",
-                    ImageUrl = "https://img3.zakaz.ua/09.1663274872.ad72436478c_2022-09-16_Victoria/09.1663274872.SNCPSG10.obj.0.1.jpg.oe.jpg.pf.jpg.1350nowm.jpg.1350x.jpg"
-                });
+                    email = "user@gmail.com";
+                    user = new DbUser
+                    {
+                        Email = email,
+                        UserName = email,
+                        FullName = "Hidan New",
+                        PhoneNumber = "+380974355116"
+                    };
+                    result = manager.CreateAsync(user, "12345678").Result;
+                    result = manager.AddToRoleAsync(user, roleName).Result;
 
-                context.CraftBeers.Add(new CraftBeer
-                {
-                    Name = "Гребуча Корова",
-                    Price = 64,
-                    Volume = 0.33,
-                    ABV = 6.0,
-                    Description = "Пиво Гребуча корова – це класичний віденський лагер, має збалансований смак, невелику гірчинку, виражений фруктовий післясмак та аромат.",
-                    ImageUrl = "https://www.beer.ua/wp-content/uploads/2022/10/Pyvo-Hrebucha-Korova.png"
-                });
+                    context.CraftBeers.Add(new CraftBeer
+                    {
+                        Name = "Bud",
+                        Price = 40,
+                        Volume = 0.5,
+                        ABV = 5.0,
+                        Description = "Пиво Bud – всесвітньо відомий король серед пива був вперше представлений Адольфусом Бушем ще у 1876 році. Відтоді й досі BUD вариться з дотриманням високих стандартів якості, незмінних з плином часу.",
+                        ImageUrl = "https://img3.zakaz.ua/09.1663274872.ad72436478c_2022-09-16_Victoria/09.1663274872.SNCPSG10.obj.0.1.jpg.oe.jpg.pf.jpg.1350nowm.jpg.1350x.jpg"
+                    });
 
-                context.CraftBeers.Add(new CraftBeer
-                {
-                    Name = "Captain Salt",
-                    Price = 66,
-                    Volume = 0.33,
-                    ABV = 5.0,
-                    Description = "На кособокій лавці у таверні, оповитий димом з потертої люльки, старий морський вовк вдихає аромат з келиха, робить ковток і чує шум бурхливих хвиль," +
-                    "крики мартинів, гомін матросів, шурхіт піску незвіданих островів під ногами, бачить напнуті вітрила і відчуває гарячі поцілунки південних красунь з далеких портів і " +
-                    "солоні долоні океану. Потроху видиво зникає. Ще один келих Captain Salt, будь ласка. Персональне море у келиху – так ми уявляли собі наш перший гозе.",
-                    ImageUrl = "https://www.beer.ua/wp-content/uploads/2022/09/Captain-Salt-Varvar-Brew.webp"
-                });
+                    context.CraftBeers.Add(new CraftBeer
+                    {
+                        Name = "Гребуча Корова",
+                        Price = 64,
+                        Volume = 0.33,
+                        ABV = 6.0,
+                        Description = "Пиво Гребуча корова – це класичний віденський лагер, має збалансований смак, невелику гірчинку, виражений фруктовий післясмак та аромат.",
+                        ImageUrl = "https://www.beer.ua/wp-content/uploads/2022/10/Pyvo-Hrebucha-Korova.png"
+                    });
 
-                context.SaveChanges();
+                    context.CraftBeers.Add(new CraftBeer
+                    {
+                        Name = "Captain Salt",
+                        Price = 66,
+                        Volume = 0.33,
+                        ABV = 5.0,
+                        Description = "На кособокій лавці у таверні, оповитий димом з потертої люльки, старий морський вовк вдихає аромат з келиха, робить ковток і чує шум бурхливих хвиль," +
+                        "крики мартинів, гомін матросів, шурхіт піску незвіданих островів під ногами, бачить напнуті вітрила і відчуває гарячі поцілунки південних красунь з далеких портів і " +
+                        "солоні долоні океану. Потроху видиво зникає. Ще один келих Captain Salt, будь ласка. Персональне море у келиху – так ми уявляли собі наш перший гозе.",
+                        ImageUrl = "https://www.beer.ua/wp-content/uploads/2022/09/Captain-Salt-Varvar-Brew.webp"
+                    });
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
             }
         }
     }
