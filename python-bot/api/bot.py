e8d5b457c60142def9d881451173210361ad7f97
Fedorenkow <oleh.yushchuk@gmail.com>
2023-09-15 10:25:06 +0300

    Create bot.py

---
+++ python-bot/api/bot.py
@@ -0,0 +1,13 @@
+from aiogram import Bot, Dispatcher, types
+import os
+
+TOKEN = '5680096668:AAHuis7pyG-G4G0taF7WeumTRdU-jgGUeY0'
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot=bot)
+
+
+@dp.message_handler(commands=['start'])
+async def start_handler(message: types.Message):
+    await message.reply(f"Hello")
+
