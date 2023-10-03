14c54a642805377bf2da810ecd5615dbe0aced10
Fedorenkow <oleh.yushchuk@gmail.com>
2023-10-03 14:15:12 +0300

    Create utils.py

---
+++ python-bot/api/utils/utils.py
@@ -0,0 +1,15 @@
+from sqlalchemy import text
+from sqlalchemy.exc import SQLAlchemyError
+
+from api.app import app, db
+
+
+def get_beer_data():
+    try:
+        with app.app_context():
+            sql_query = text('SELECT * FROM "CraftBeers"')
+            result = db.session.execute(sql_query)
+            beer_data = result.fetchall()
+            return beer_data
+    except SQLAlchemyError as e:
+        return str(e)
