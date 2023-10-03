e7ca90529ebbd5cda2f806492abdb2a0f773e39c
Fedorenkow <oleh.yushchuk@gmail.com>
2023-10-03 14:15:26 +0300

    Update bot.py

--- python-bot/api/bot.py
+++ python-bot/api/bot.py
@@ -1,7 +1,6 @@
 from aiogram import Bot, Dispatcher, types
 
-from api.app import app
-from api.models.CraftBeer import CraftBeer
+from api.utils.utils import get_beer_data
 
 TOKEN = '5680096668:AAHuis7pyG-G4G0taF7WeumTRdU-jgGUeY0'
 
@@ -14,13 +13,18 @@ async def start_handler(message: types.Message):
     await message.reply(f"Hello")
 
 
-@app.message_handler(commands=['beers'])
+@dp.message_handler(commands=['beers'])
 async def beer_info_answer(message: types.Message):
-    with app.app_context():
-        beers = CraftBeer.query.all()
+    beer_data = get_beer_data()
 
-        beer_info = ""
-        for beer in beers:
-            beer_info += f"ID: {beer.id}\nName: {beer.name}\nDescription: {beer.description}\nPrice: {beer.price}\n\n"
+    if isinstance(beer_data, str):
+        # Обробка помилок бази даних
+        await message.reply(f"Помилка бази даних: {beer_data}")
+    else:
+        # Створіть рядок з отриманими даними
+        response_text = ""
+        for beer in beer_data:
+            response_text += f"ID: {beer[0]} \n Name: {beer[1]}, \n Description: {beer[2]}\n\n"
 
-        await message.reply(beer_info)
+        # Відправте відповідь користувачу
+        await message.reply(response_text)
