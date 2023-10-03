f788228d911e77d3a16475a2c4be8a1cfca12295
Fedorenkow <oleh.yushchuk@gmail.com>
2023-10-03 15:03:39 +0300

    Update bot.py

--- python-bot/api/bot.py
+++ python-bot/api/bot.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher, types
 
-from api.utils.utils import get_beer_data
+from api.utils.utils import get_beer_data, get_order_data
 
 TOKEN = '5680096668:AAHuis7pyG-G4G0taF7WeumTRdU-jgGUeY0'
 
@@ -18,13 +18,23 @@ async def beer_info_answer(message: types.Message):
     beer_data = get_beer_data()
 
     if isinstance(beer_data, str):
-        # Обробка помилок бази даних
         await message.reply(f"Помилка бази даних: {beer_data}")
     else:
-        # Створіть рядок з отриманими даними
         response_text = ""
         for beer in beer_data:
             response_text += f"ID: {beer[0]} \n Name: {beer[1]}, \n Description: {beer[2]}\n\n"
 
-        # Відправте відповідь користувачу
+        await message.reply(response_text)
+
+@dp.message_handler(commands=['order'])
+async def beer_info_answer(message: types.Message):
+    order_data = get_order_data()
+
+    if isinstance(order_data, str):
+        await message.reply(f"Помилка бази даних: {order_data}")
+    else:
+        response_text = ""
+        for order in order_data:
+            response_text += f"ID: {order[0]} \n Unique Code: {order[1]}, \n Date: {order[2]} \n User: {order[3]} \n Status: {order[4]} \n Customer: {order[5]} \n Delivery: {order[6]}\n\n"
+
         await message.reply(response_text)
