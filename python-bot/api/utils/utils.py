f7fca19370172343ba83a069986fa066b9a8ef7b
Fedorenkow <oleh.yushchuk@gmail.com>
2023-10-08 17:16:48 +0300

    Update utils.py

--- python-bot/api/utils/utils.py
+++ python-bot/api/utils/utils.py
@@ -17,9 +17,28 @@ def get_beer_data():
 def get_order_data():
     try:
         with app.app_context():
-            sql_query = text('SELECT * FROM "Orders"')
+            sql_query = text('''
+                SELECT
+                    o."Id" AS "OrderId",
+                    o."UniqueCode" AS "OrderUniqueCode",
+                    o."Date" AS "OrderDate",
+                    os."Name" AS "OrderStatus",
+                    ci."FirstName" AS "CustomerFirstName",
+                    ci."LastName" AS "CustomerLastName",
+                    ci."Phone" AS "CustomerPhone",
+                    da."City" AS "DeliveryCity",
+                    da."Address" AS "DeliveryAddress"
+                FROM
+                    "Orders" AS o
+                LEFT JOIN
+                    "OrderStatuses" AS os ON o."OrderStatusId" = os."Id"
+                LEFT JOIN
+                    "CustomerInfos" AS ci ON o."CustomerInfoId" = ci."Id"
+                LEFT JOIN
+                    "DeliveryAddresses" AS da ON o."DeliveryAddressId" = da."Id"
+            ''')
             result = db.session.execute(sql_query)
-            beer_data = result.fetchall()
-            return beer_data
+            order_data = result.fetchall()
+            return order_data
     except SQLAlchemyError as e:
         return str(e)
