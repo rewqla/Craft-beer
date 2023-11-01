d2784a78acb1f2c335afbb48c280d61d51a3d6be
Vitalik Yashchuk <yashchuk_ak21@nuwm.edu.ua>
2023-11-01 14:51:47 +0200

    add front

---
+++ beer-site-frontend/js/main.js
@@ -0,0 +1,30 @@
+
+var modal = document.getElementById("sign-in-modal");
+var btn = document.getElementById("sign-in-button");
+btn.onclick = function() {
+  modal.style.display = "flex";
+}
+
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+var regLink = document.getElementById("reg-link");
+var signUpModal = document.getElementById("sign-up-modal");
+regLink.onclick = function() {
+    modal.style.display = "none";
+    signUpModal.style.display = "flex";
+}
+var signInLink = document.getElementById("sign-in-link");
+signInLink.onclick = function(){
+    signUpModal.style.display = "none";
+    modal.style.display = "flex";
+}  
+window.onclick = function(event){
+    if (event.target == signUpModal || event.target == modal) {
+        signUpModal.style.display = "none";
+        modal.style.display = "none";      
+    }
+}
