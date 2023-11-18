1a2aaf1568d4459330ea3ef4ecd9a709966645ab
Фугель Владислав <102908868+Fyhel-Vladyslav@users.noreply.github.com>
2023-11-18 03:04:42 +0200

    Add correct searching in layout

--- Craft-beer-backend/Repositories/Implements/CraftBeerRepository.cs
+++ Craft-beer-backend/Repositories/Implements/CraftBeerRepository.cs
@@ -1,13 +1,22 @@
 ﻿using Craft_beer_backend.Entities;
 using Craft_beer_backend.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Craft_beer_backend.Repositories.Implements
 {
     public class CraftBeerRepository : GenericRepository<CraftBeer>, ICraftBeerRepository
     {
-        public CraftBeerRepository(ApplicationDbContext context) : base(context)
+        private DbContextOptions<ApplicationDbContext> options;
+        public CraftBeerRepository(ApplicationDbContext context, DbContextOptions<ApplicationDbContext> options) : base(context)
         {
+            this.options = options;
+        }
 
+        public List<string> GetNames()
+        {
+           return new ApplicationDbContext(options).CraftBeers.Select(p=>p.Name).ToList();
         }
     }
 }
--- Craft-beer-backend/Repositories/Interfaces/ICraftBeerRepository.cs
+++ Craft-beer-backend/Repositories/Interfaces/ICraftBeerRepository.cs
@@ -1,8 +1,10 @@
 ﻿using Craft_beer_backend.Entities;
+using System.Collections.Generic;
 
 namespace Craft_beer_backend.Repositories.Interfaces
 {
     public interface ICraftBeerRepository : IGenericRepository<CraftBeer>
     {
+        public List<string> GetNames();
     }
 }
