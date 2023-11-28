85540195e14d9b72743498f9ac4c5bd16830b750
rewqla <desares526@gmail.com>
2023-11-28 17:50:07 +0200

    Update the CraftBeer entity and the seeder

--- Craft-beer-backend/Migrations/20231128154512_Add short decription field to the craft beer.Designer.cs
+++ Craft-beer-backend/Migrations/20231128154512_Add short decription field to the craft beer.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Craft_beer_backend.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Craft_beer_backend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231128154512_Add short decription field to the craft beer")]
+    partial class Addshortdecriptionfieldtothecraftbeer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
